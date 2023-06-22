f092e19bcdb633745d32b76039292778232f24d3
Himanshu-sudo <himanshudhakate07@gmail.com>
2023-06-22 15:12:16 +0530

    Filed changed

--- Arrays/test.cpp
+++ Arrays/test.cpp
@@ -2,16 +2,21 @@
 using namespace std ; 
 
 int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("/home/himanshu/Programming/input.txt", "r", stdin);
+        freopen("/home/himanshu/Programming/output.txt", "w", stdout);
+    #endif
+    
     int arr[5] ; 
-    cout<<"Enter element inside the array " ; 
+    //cout<<"Enter element inside the array " ; 
     for(int i = 0 ; i < 5 ; i++){
         cin>>arr[i] ; 
     }
 
-    cout<<"The elements of the array are " ; 
+    //cout<<"The elements of the array are " ; 
 
     for(int i = 0 ; i < 5 ; i++){
-        cout<<arr[i]<<" " ;
+        cout<<3 * arr[i]<<" " ;
     }
     cout<<endl ; 
     return 0 ; 
