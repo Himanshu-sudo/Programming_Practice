94132a64028e4ccdac5b1470be184b293f9a487f
Himanshu-sudo <himanshudhakate07@gmail.com>
2023-12-14 22:23:12 +0530

    Optimized

--- Bit Manipulation/XOR_challenge.cpp
+++ Bit Manipulation/XOR_challenge.cpp
@@ -11,18 +11,11 @@ int main() {
         cout << "0\n";
     }
     else {
-        int power = 4;
-        int no_of_bits = 2;
-        
-        while (c >= power) {
-            no_of_bits++;
-            power <<= 1;
-        }
+        int no_of_bits = (int)log2(c) + 1;
 
         int first_number = (1 << (no_of_bits - 1)) - 1;
         int second_number = c ^ first_number;
-        // cout << no_of_bits << "\n";
-        // cout << first_number << " " << second_number << "\n";
+       
         cout << first_number * 1LL * second_number << "\n";
     }
     return 0;
